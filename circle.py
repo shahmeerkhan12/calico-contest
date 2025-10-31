55e781e97faf987cde00e83b7112949229751f6a
Shahmir Khan <kshahmir822@gmail.com>
2025-10-31 23:02:33 +0500

    first commit

---
+++ circle.py
@@ -0,0 +1,28 @@
+def solve(N: int, X: list[float], Y: list[float]) -> float:
+    """
+    Return the area of the rectangle described by the given points.
+    
+    N: the number of given points
+    X: a list containing the x-coordinate of each point
+    Y: a list containing the y-coordinate of each point
+    """
+    # YOUR CODE HERE
+    width = 0
+    height = 0
+    width = max(X) - min(X)
+    height = max(Y) - min(Y)
+    return width*height
+        
+    return 0.0
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        X, Y = zip(*(map(float, input().split()) for _ in range(N)))
+        print(solve(N, X, Y))
+
+
+if __name__ == '__main__':
+    main()
