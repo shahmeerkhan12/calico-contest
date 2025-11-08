2a357102b5716b343ba426da5e512c2672417751
Shahmir Khan <kshahmir822@gmail.com>
2025-11-08 13:17:40 +0500

    here we go with the champ of the tournament

--- calico Sping-25/tournament_champ.py
+++ calico Sping-25/tournament_champ.py
@@ -1,27 +1,4 @@
-competitors = input().strip().split()
-powers = list(map(int,input().strip().split(" ")))
-
-# print(type(powers))
-champion = [(competitors[i],powers[i]) for i in range(len(powers))]
-
-while len(champion)>1:
-   nextRound =[]
-   for i in range(0,len(champion),2):
-      comp1, comp2 = champion[i],champion[i+1]
-      if comp1[1]>comp2[1]:
-         nextRound.append((comp1[0],int(comp1[1]+comp2[1])))
-      elif comp1[1]<comp2[1]:
-         nextRound.append((comp2[0],int(comp1[1]+comp2[1])))
-      else:
-         superGuy = comp1[0]+comp2[0]
-         # but I need to add the powers too, and add to the newly superGuy
-         superPower= int(comp1[1]+comp2[1])
-         nextRound.append((superGuy,superPower))
-   champion = nextRound
-
-print(champion[0][0])
-
-# def solve(N: int, C: list, P: list) -> str:
+def solve(N: int, C: list, P: list) -> str:
 #     """
 #     Return a single string of the champion's name
     
@@ -29,28 +6,51 @@ print(champion[0][0])
 #     C: List of strings of the competitors
 #     P: List of integers of competitor's power
 #     """
-#     # YOUR CODE HERE
-#     # champ = C[P.index(max(P))]
-#     champ  = list()
+# SOLUTION:
+      # let me make pairs of the competitors to fight each other
+      comp_pairs = [(C[i],P[i]) for i in range(N)]
+      # DO THE FOLLOWING TILL:
+      while len(comp_pairs)>1:
+          
+          # will store the intermediate champions for the next round of fight
+          nextRound = []
+          for i in range(0,len(comp_pairs),2):
+              # now i will have two competitors
+              competitor1, competitor2 = comp_pairs[i],comp_pairs[i+1]
+              
+            # compare comp1 and comp2
+              if competitor1>competitor2:
+                  # add powers
+                  power = competitor1[1] + competitor2[1]
+                  # update the nextRound with:
+                  nextRound.append((competitor1[0],power))
+              elif competitor1<competitor2:
+                  power = competitor1[1] + competitor2[1]
+                  nextRound.append((competitor2[0],power))
+              else:
+                  # we got the same powers so:
+                  superFighter = competitor1[0] + competitor2[0]
+                  superPower = competitor1[1]+ competitor2[1]
+                  nextRound.append((superFighter,superPower))
 
-#     for i in P:
-#         if i>=i+1:
-#             P.pop(i+1)
-#         elif i<=i+1:
-#             P.pop()
+          comp_pairs = nextRound
+          # i just need the name of the champion
+      champion = comp_pairs[0][0]
+      
+      return champion
+                 
 
 #     return champ
-# # try this also
-#     # return C[P.index(max(P))]
 
 
-# def main():
-#     T = int(input())
-#     for _ in range(T):
-#         N = int(input())
-#         C = input().split(" ")
-#         P = list(map(lambda s: int(s), input().split(" ")))
-#         print(solve(N, C, P))
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input()) 
+        C = input().split(" ")
+        P = list(map(lambda s: int(s), input().split(" ")))
+        print(solve(N, C, P))
+
+if __name__ == '__main__':
+    main()
